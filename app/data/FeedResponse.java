c2569ce5f3f25c9de4295c890b43d8cc0e09caf9
mandarasp <mandarasp5@gmail.com>
2018-04-14 07:28:38 +0000

    added fedresponse

--- app/data/FeedResponse.java
+++ app/data/FeedResponse.java
@@ -2,4 +2,8 @@ package data;
 
 public class FeedResponse {
 
+    public String title;
+    public String description;
+    public String pubdate;
+
 }
--- app/services/FeedService.java
+++ app/services/FeedService.java
@@ -11,5 +11,23 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 public class FeedService {
+ public FeedResponse getFeedByQuery(String query){
+     FeedResponse feedresponse= new FeedResponse();
+     try{
+         WSRequest queryRequest=ws.url("http://news.google.com/news");
+         CompletionStage<WSReponse>responsePromise=queryRequest
+                 .setQueryParameter(name:"q",value:"srh")
+               .setQueryParameter("output","rss")
+             .get();
+     Document feedresponse =responsePromise.thenApply(WSResponse::asXml).toCompletableFuture().get();
+     Node item=feedresponse.getFirstChild().getChildNodes().item(index:10);
+     feedresponseobj.title=item.getChildNodes().item(index:0).getNodeValue();
+     feedresponseobj.pubdate=item.getChildNodes().item(index:3).getNodeValue();
+     feedresponseobj.description=item.getChildNodes().item(index:4).getNodeVlue();
 
+ }
+       catch(Exception e){
+        e.printStackTrace();
+    }
+    return feedResponseObject;
 }
